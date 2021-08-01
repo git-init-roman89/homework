66fd730d8f4e28961145eaaf51ba2b93b34688ab
Roman <mailto.32roman@gmail.com>
2021-08-01 21:06:43 +0300

    removed homework_1.js from index

---
+++ homework_2.js
@@ -0,0 +1,12 @@
+// Homework 2, task 1
+
+const logItems = function(array) {
+    for (let i = 1; i < array.length; i = i + 1) {
+        let output = "i " +  array[i];
+        console.log(output);
+  };
+}
+
+  logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+  
+//   logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
